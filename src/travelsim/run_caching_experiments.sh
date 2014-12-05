557d526c068455580d92884ea2ffd8353bb6ce16
Jithin Thomas <jithinpt@dn525aoo.sunet>
2014-12-05 14:48:19 -0800

    Minor changes to run_caching_experiments.sh

--- src/travelsim/run_caching_experiments.sh
+++ src/travelsim/run_caching_experiments.sh
@@ -1,19 +1,19 @@
 
 # Manual network simulation with constant distribution for trip interval and count and network modifications enabled
-client-manual-network-sim 1418385 360 1 1 1 > manual_1_1_1.txt
+time ./client-manual-network-sim 1418385 360 1 1 1 > manual_1_1_1.txt
 
 # Manual network simulation with uniform or normal distribution for trip interval and count and network modifications enabled
-client-manual-network-sim 1418385 360 1 0 1 > manual_1_0_1.txt
+time ./client-manual-network-sim 1418385 360 1 0 1 > manual_1_0_1.txt
 
 # Manual network simulation with constant distribution for trip interval and count and network modifications disabled
-client-manual-network-sim 1418385 360 0 1 1 > manual_0_1_1.txt
+time ./client-manual-network-sim 1418385 360 0 1 1 > manual_0_1_1.txt
 
 # Manual network simulation with uniform or normal distribution for trip interval and count and network modifications disabled
-client-manual-network-sim 1418385 360 0 0 1 > manual_0_0_1.txt
+time ./client-manual-network-sim 1418385 360 0 0 1 > manual_0_0_1.txt
 
 # Very dense network with network modification activity enabled
-time ./client-auto-network-sim 200 30000 200 1 23901846 360 1 > very_dense_nm_1_cache_1.txt
-time ./client-auto-network-sim 200 30000 200 1 23901846 360 0 > very_dense_nm_1_cache_0.txt
+time ./client-auto-network-sim 400 80000 200 1 23901846 240 1 > very_dense_nm_1_cache_1.txt
+time ./client-auto-network-sim 400 80000 200 1 23901846 240 0 > very_dense_nm_1_cache_0.txt
 
 # Dense network with network modification activity enabled
 time ./client-auto-network-sim 200 15000 200 1 21478797 360 1 > dense_nm_1_cache_1.txt
